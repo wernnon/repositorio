47d86e46b1ecb39b74a1d2b3a82011aad1eae259
wernnon <ruizwernnon@gmail.com>
2022-06-24 18:13:20 -0400

    pagina covid

--- src/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-function HelloWorld(){
-    return(
-        <h1>Seu armando</h1>
-    );
-}
-
-export default HelloWorld;
--- src/Title.jsx
+++
@@ -1,7 +0,0 @@
-import './Title.css'
-function Title(){
-    return(
-        <h1 className="title">Talves você conheça</h1>
-    )
-}
-export default Title;
