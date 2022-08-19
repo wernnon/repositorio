f03bb482a04671d2711a1e87189b331187b5a2ad
Felipe-smg <felipecambitinho@gmail.com>
2022-08-19 14:36:28 -0400

    atividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,99 +3,21 @@ import './App.css'
 
 function App(){
 return(
-  <div className='body'>
-    <div className='corpo'>
-      <div className='box'>
-      <div className='box1'>
-        Processos
-      </div>
-      <div className='descr'>
-        Processos relacinado ao combate do COVID-19
-      </div>
-      </div>
-      <div className='box'>
-        <div className='box1'>
-          Contatos
+  <div>
+    <div className='main'>
+      <div className='inform'>
+        <div className='title'>
+          titulo
         </div>
-        <div className='descr'>
-          Contatos relacionados ao combate do COVID-19
+        <div className='news'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium vero asperiores fugiat architecto officiis, nesciunt illum? Omnis provident quos illo, doloremque nisi ullam tempore veniam. Animi nobis blanditiis quidem dolore!
         </div>
       </div>
+      <div className='mainImg'>
+          <img src='src\1140x641.jpg' height={440}width={740}/>
+        </div>
     </div>
-    <div className='corpo'>
-        <div className='box'>
-          <div className='box2'>
-            Licitações e compras diretas(Dispesas)
-          </div>
-          <div className='descr'>
-            Compras destinadas ao enfrentamento e combate a COVID-19
-          </div>
-        </div>      
-    </div>
-    <div className='corpo'>
-    <div className='box'>
-      <div className='box3'>
-      Receitas Federais
-      </div>
-      <div className='descr'>
-      Receitas Federais destinadas ao bombate a COVID-19
-      </div>
-      <div className='valor'>
-        Arrecadada: 73.082,40
-      </div>
-      </div>
-      <div className='box'>
-        <div className='box3'>
-          Receitas Estaduais
-        </div>
-        <div className='descr'>
-          receitas etaduais destinadas ao bombate a COVID-19
-        </div>
-        <div className='valor'>
-        Arrecadada: 0,00
-        </div>
-      </div>
-    </div>
-    <div className='corpo'>
-      <div className='box'>
-        <div className='box4'>
-          Empenhos
-        </div>
-        <div className='descr'>
-          <p>Despesas empenhadas destinadas ao enfrentamento e</p> 
-          combate a COVID-19
-        </div>
-        <div className='valor'>
-          <p>Valor atualizado: 0,00</p>
-          Restos: 32.338,04
-        </div>
-      </div>
-      <div className='box'>
-        <div className='box4'>
-          Liquidações
-        </div>
-        <div className='descr'>
-          <p>Despesas liquidadas destinadas ao enfrentamento e</p> 
-          combate a COVID-19
-        </div>
-        <div className='valor'>
-          Valor atualizado: 20.137,02
-        </div>
-      </div>
-      <div className='box'>
-        <div className='box4'>
-          Pagamentos
-        </div>
-        <div className='descr'>
-          <p>Despesas pagas destinadas ao enfrentamento e</p> 
-          combate a COVID-19
-        </div>
-        <div className='valor'>
-          Valor atualizado: 20.137,02
-        </div>
-      </div>
-    </div>
-  </div> 
+  </div>
 )
 }
 export default App
