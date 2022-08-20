fe5456028822e4122354874a5b5537706fc52fde
wernnon <ruizwernnon@gmail.com>
2022-08-19 22:57:45 -0400

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ return(
           titulo
         </div>
         <div className='news'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, voluptas recusandae obcaecati impedit soluta rerum, molestiae repudiandae dolorum, fugiat nihil excepturi voluptatum ut dolore minus facere. Harum nemo odit fuga.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium vero asperiores fugiat architecto officiis, nesciunt illum? Omnis provident quos illo, doloremque nisi ullam tempore veniam. Animi nobis blanditiis quidem dolore!
         </div>
       </div>
       <div className='mainImg'>
-          <img src='src\1140x641.jpg' height={440}width={740}/>
+          <img className='topimg' src='src\1140x641.jpg'/>
         </div>
     </div>
   </div>
