5a4c6a81bdaee3072d31eed677ade3fa6ba84824
wernnon <ruizwernnon@gmail.com>
2022-08-26 14:47:43 -0400

    finalização da componetização

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ return(
       news="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis id cupiditate aliquid eos! Aut illum impedit ullam voluptas placeat incidunt, corrupti laudantium, atque porro ab molestias quas ut. Sequi, in!" 
       image="src/Images/1140X641.jpg" 
       />
-      <Sugest image="src/Images/1140X641.jpg" />
+      <Sugest/>
 
 
       
--- src/Componetes/Sugest/sugest.jsx
+++ src/Componetes/Sugest/sugest.jsx
@@ -1,19 +1,50 @@
 import React from 'react'
+import './sugest.css'
 
-export default function Sugest(props) {
-  return (
-    <div className='sugest'>
-        <div className='box'>
-            <div className='secImg'>
-                <img className='botImg' src={props.image}/>
+export default function Sugest() {
+    const info = [
+        {
+            img: 'src/Images/1140x641.jpg',
+            titulo : 'place holder'
+        },
+        {
+            img: 'src/Images/1140x641.jpg',
+            titulo : 'place holder'
+        },
+        {
+            img: 'src/Images/1140x641.jpg',
+            titulo : 'place holder'
+        },
+        {
+            img: 'src/Images/1140x641.jpg',
+            titulo : 'place holder'
+        },
+        {
+            img: 'src/Images/1140x641.jpg',
+            titulo : 'place holder'
+        },
+    ]
+    const sugest = info.map(
+        (c)=> 
+        
+            <div className='box'>
+                <div className='Img'>
+                    <img className='Img'src={c.img}/>
+                </div>
+                <div className='title'>
+                    {c.titulo}  
+                </div>
             </div>
-            <div className='title2'>
-                {props.titulo}
-            </div>
+      )
+    return (
+    <div>
+        <div className='sugest'>
+            {sugest}
         </div>
+        
     </div>
   )
-}
+};
 
 
 
