2fa1aaf9c733ea07cbeea2197d7abbc5efed094d
guxtavinho69 <gustavoruiz637@gmail.com>
2022-09-16 14:25:45 -0400

    tentativa do .map no footer

--- src/Componentes/Footer/footer.jsx
+++ src/Componentes/Footer/footer.jsx
@@ -1,20 +1,57 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
+import './Footercss.css'
+
 
 function Footer(){
+    const Footer = [{
+        img: <img src='src/Images/infro.jpg' width={15}></img>,
+        item: "Escola"
+    },
+    {
+        img: <img src='src/Images/instagram.jpg' width={15}></img>,
+        item: "Instagran"   
+    },
+    {
+        img: <img src='src/Images/facebook.png' width={15}></img>,
+        item: "Facebook"
+    }
+]
+
+const rodape = Footer.map(
+    (rodape)=>
+    {
+        return(
+        <div className='lista'>
+            {img.map}
+            <br></br>
+            {item.map}
+        </div>
+        )
+    }
+)
 return(
+    
     <footer className='Footer'>
-        <ul>
+        <ul className='lista'>
             <li>
-                Footer 
+            <img src='src/Images/infro.jpg' width={15}></img>
+                Escola
             </li>
             <li>
-                Footer 
+            <img src='src/Images/instagram.jpg' width={15}></img>
+                Instagram  
             </li>
             <li>
-                Footer 
+            <img src='src/Images/facebook.png' width={15}></img>
+                Facebook 
             </li>
+            
         </ul>
+        <div>
+            {rodape}
+        </div>
+        
     </footer>
 )
 }
